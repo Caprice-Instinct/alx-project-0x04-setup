f25b67ad3bf0b067484fa230cc0b027b24c39f1b
Caprice-Instinct <linetwangui04@gmail.com>
2025-11-30 19:36:31 +0300

    first commit

---
+++ alx-project-0x04/interface/index.tsx
@@ -0,0 +1,20 @@
+// Centralized interfaces for the project
+import { ReactNode } from "react";
+
+// From pages/index.tsx
+export interface PageRouteProps {
+  pageRoute: string;
+}
+
+// From components/common/Button.tsx
+export interface ButtonProps {
+  buttonLabel: string;
+  buttonSize?: string;
+  buttonBackgroundColor?: "red" | "blue" | "orange" | "green";
+  action?: () => void;
+}
+
+// From components/layouts/Layout.tsx
+export interface LayoutProps {
+  children: ReactNode;
+}
